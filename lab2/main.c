785ace92207606b65983b698bcaf749706e29a24
daniel <119851639+chizuru-Ichin0se@users.noreply.github.com>
2025-02-17 17:35:37 +1000

    Create main.c

---
+++ lab2/main.c
@@ -0,0 +1,30 @@
+int main() {
+	int n; 
+	
+	printf("Razmer matrici: \n");
+	scanf("%d", &n);
+	
+	double matrix[n][n];
+	
+	printf("Matrica(n x n):\n");
+	int i, j;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++){
+			scanf("%lf", &matrix[i][j]);
+		}
+	}
+	
+	double mainn = 0.0;
+	double dop = 0.0;
+	
+	for (i = 0; i < n; i++){
+		mainn += matrix[i][i];
+		dop += matrix[i][n-1-i];
+	}
+	
+	printf("Glavnaya: %.2lf", mainn);
+	printf("Pobochnaya: %.2lf", dop);
+	
+	
+	return 0;
+}
