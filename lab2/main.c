3d6393c1f1392631c82856d34027461b624deb57
daniel <119851639+chizuru-Ichin0se@users.noreply.github.com>
2025-02-17 18:39:27 +1000

    Add files via upload

---
+++ lab2/main.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define n 2
+int main() {
+  
+  int matrix[n][n];
+  
+  printf("Matrica(n x n):\n");
+  int i, j;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++){
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+  
+  int kvmatrix[n][n] = {0};
+  
+  int k;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      for (k = 0; k < n; k++) {
+        kvmatrix[n][n] += matrix[i][k] * matrix[k][j];
+      }
+    }
+  }
+  
+  printf("Kvadrat\n");
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      printf("%d ", kvmatrix[i][j]);
+    }
+    print("\n");
+  }
+  
+  return 0;
+}
+
