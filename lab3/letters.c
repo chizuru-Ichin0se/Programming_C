c80c064a37a721ac14c97ab58969e5203e78a05c
daniel <119851639+chizuru-Ichin0se@users.noreply.github.com>
2025-02-19 17:52:34 +1000

    Create letters.c

---
+++ lab3/letters.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {  
+  char str[81];
+  printf("do 80 simvolov: ");
+  fgets(str, sizeof(str), stdin);
+  int i;
+  for (i = 0; str[i] != '0'; i++) {
+    if (str[i] == 'a') {
+      str[i] = 'A';
+    } 
+    else if (str[i] == 'b') {
+      str[i] = 'B';
+    }
+  }
+  
+  
+  printf("vivod: %s", str);
+  
+  return 0;
+}
